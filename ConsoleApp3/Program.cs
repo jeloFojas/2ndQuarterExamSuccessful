27f7b4d8d3579438f64913edcf5852dbd234e1d7
jeloFojas <jelo.fojas@deped.gov.ph>
2025-10-04 23:47:24 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			string projectStatus = "Project: Exam v1.0";
+			int totalQuestions = 50;
+
+			for (int answeredQuestions = 0; answeredQuestions <= totalQuestions; answeredQuestions++)
+			{
+				if (answeredQuestions == totalQuestions)
+				{
+					Console.WriteLine($"// {projectStatus} - Question {answeredQuestions} of {totalQuestions} **ANSWERED**.");
+					Console.WriteLine("Git.Commit(\"Final Answer\");");
+					Console.WriteLine("Git.Push(\"To Grader\");");
+				}
+				else answeredQuestions++;
+			}
+			// System.Exit(0) - Process terminated successfully. **REST MODE ACTIVATED.**
+
+		}
+	}
+}
