e6722af2a59b316278d2f906dc4ff2ac5004a45c
jeloFojas <jelo.fojas@deped.gov.ph>
2025-10-05 10:45:33 +0800

    update

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;	
+using System.ComponentModel;
+using System.Threading;
 
 namespace ConsoleApp3
 {
@@ -6,18 +8,28 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-			string projectStatus = "Project: Exam v1.0";
+			Random random = new Random();
+			string examSecondQuarter = "2nd Periodic Exam";
 			int totalQuestions = 50;
+
 
 			for (int answeredQuestions = 1; answeredQuestions <= totalQuestions; answeredQuestions++)
 			{
 				if (answeredQuestions == totalQuestions)
 				{
-					Console.WriteLine($"// {projectStatus} - Question {answeredQuestions} of {totalQuestions} **ANSWERED**.");
+					Console.WriteLine($"// {examSecondQuarter} - Question {answeredQuestions} of {totalQuestions} **ANSWERED**.");
 					Console.WriteLine("Git.Commit(\"Final Answer\");");
 					Console.WriteLine("Git.Push(\"To Grader\");");
+					Console.WriteLine("Congratulations!");
+					Console.WriteLine("See you maybe on next sem!");
+					Console.ReadLine();
 				}
-				else answeredQuestions++;
+				else
+				{
+					Console.WriteLine($"Answering Question {answeredQuestions}...");
+					Thread.Sleep(1000);
+				}
+
 			}
 			// System.Exit(0) - Process terminated successfully. **REST MODE ACTIVATED.**
 
